2a05e64ad3d5f732a0c5d682ac483fdb7fb2c320
Anand Varma <anand.pyro@gmail.com>
2024-02-01 00:40:19 -0500

    Initial Commit

---
+++ SleepTimer/SleepTimerApp.swift
@@ -0,0 +1,17 @@
+//
+//  SleepTimerApp.swift
+//  SleepTimer
+//
+//  Created by Anand on 2/1/24.
+//
+
+import SwiftUI
+
+@main
+struct SleepTimerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
